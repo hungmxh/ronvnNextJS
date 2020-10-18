d8a99d220beaf379766616af59c802e24f0b5dcc
hungmxh <hungmxh@gmail.com>
2020-10-18 15:43:26 +0700

    Update post-body.js

--- components/post-body.js
+++ components/post-body.js
@@ -1,10 +1,23 @@
 import markdownStyles from './markdown-styles.module.css'
-import BlockContent from '@sanity/block-content-to-react'
+import PortableText from '@sanity/block-content-to-react'
+import { imageBuilder } from '../lib/sanity'
+
 
 export default function PostBody({ content }) {
+  const serializer = {
+    types: {
+      image: props => (
+        <img
+          alt={props.node.alt +'alt text'}
+          src={imageBuilder.image(props.node.asset).url()}
+        />
+      )
+    }
+  };
+
   return (
     <div className="max-w-2xl mx-auto">
-      <BlockContent blocks={content} className={markdownStyles.markdown} />
+      <PortableText blocks={content} className={markdownStyles.markdown} serializers={serializer}/>
     </div>
   )
 }
